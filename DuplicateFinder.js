66bbdcc8a316ed7ba69bf01ce1808e7f29553a75
techSatabdi <151060021+techSatabdi@users.noreply.github.com>
2023-11-16 22:33:07 +0530

    Code to find no of count of char in a string

--- DuplicateFinder.js
+++ DuplicateFinder.js
@@ -1,1 +1,31 @@
-//abc 
+// To find the character count in a string
+
+let str = "satabdimahanta";\
+
+function occuranceCntCheck(data){
+if(data.length > 0 && !Array.isArray(data)){
+  let splittedData = str.split('');
+  let sortedData = splittedData.sort();
+  console.log(sortedData);
+  let current = null;
+  let cnt = 0;
+  for(var i = 0; i<sortedData.length; i++){
+  	if(sortedData[i]!=current){
+    	if(cnt > 0){
+      	console.log(current +" comes: " +cnt +" times")
+      }
+    	current = sortedData[i];
+      cnt = 1;
+    }else{
+    	cnt++
+    }
+  }
+  if(cnt > 0){
+    console.log(current +" comes: " +cnt+ " times")
+  }
+}else{
+	alert("Not a valid input")
+}
+
+}
+occuranceCntCheck(str);
